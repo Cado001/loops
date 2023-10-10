3a22b25b8e368f341876e2d57b2810b0b99aa002
Micado Pierre <Biml16gb@gmail.com>
2023-10-10 10:53:22 -0400

    loops through array

---
+++ challenge.js
@@ -0,0 +1,8 @@
+const  videos = ['cats','news','games','sports']
+// console log each of these videos to the screen 
+// make sure it works for any length of array
+
+
+for(let i = 0; i < videos.length; i++){
+    console.log(videos[i])
+}
---
+++ loops.js
@@ -0,0 +1,20 @@
+// Say hello 3 times
+for (let i = 0;i < 3; i++){
+  console.log('hello')
+}
+/*
+0
+1
+2
+3
+*/
+for(let i = 0; i < 4; i++){
+  console.log(i)
+}
+
+const people = ['todd', 'andrew','derek', 'Cado']
+// make a loop that says hello to each person in the array
+
+for(let i = 0; i < people.length; i++){
+    console.log('hello ' + people[i])
+}
